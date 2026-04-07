caf0932b68e1d1783675dbef2b593a44db57e563
Saket Kumar <110307134+burnwalsaket@users.noreply.github.com>
2026-04-07 23:52:30 +0530

    band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city_name = input(" Which city you grew up in ")
+pet_name = input(" What's your pet name? ")
+#printing the band name
+print("Your band name could be:"+city_name+pet_name)
